13480e240aaa27aa60a76dcf7fae90c3390d48a5
mt523 <mtostenson@mail.csuchico.edu>
2014-08-27 08:18:43 -0700

    Camera activity gets coordinates.

--- src/com/bustr/activities/CameraActivity.java
+++ src/com/bustr/activities/CameraActivity.java
@@ -1,7 +1,11 @@
 package com.bustr.activities;
 
 import android.annotation.TargetApi;
+import android.content.Context;
 import android.hardware.Camera;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -9,29 +13,37 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
+import android.widget.TextView;
+
 import com.bustr.R;
-import com.bustr.R.id;
-import com.bustr.R.layout;
-import com.bustr.R.menu;
 import com.bustr.utilities.CameraPreview;
 
-public class CameraActivity extends ActionBarActivity {
+public class CameraActivity extends ActionBarActivity implements LocationListener {
 
     private final static String LOGTAG = "BUSTR";
     
     private Camera mCamera;
     private CameraPreview mPreview;
+    private LocationManager locationManager;    
+    
+    private TextView lat_long_view;
     
     @TargetApi(Build.VERSION_CODES.GINGERBREAD)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        lat_long_view = (TextView)findViewById(R.id.lat_long_view);
+        
         mCamera = getCameraInstance();        
         mPreview = new CameraPreview(this, mCamera);
         FrameLayout preview = (FrameLayout)findViewById(R.id.camera_preview);
         CameraPreview.setCameraDisplayOrientation(this, Camera.CameraInfo.CAMERA_FACING_FRONT, mCamera);
         preview.addView(mPreview);
+        
     }
     
     @Override
@@ -69,6 +81,31 @@ public class CameraActivity extends ActionBarActivity {
             Log.d(LOGTAG, e.toString());
         }
         return c;
+    }
+
+    @Override
+    public void onLocationChanged(Location loc) {
+        String lat_long = "lat: " + loc.getLatitude() + "\nlong: " + loc.getLongitude();
+        Log.d(LOGTAG, lat_long);
+        lat_long_view.setText(lat_long);
+    }
+
+    @Override
+    public void onProviderDisabled(String arg0) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void onProviderEnabled(String arg0) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+        // TODO Auto-generated method stub
+        
     }
     
     
