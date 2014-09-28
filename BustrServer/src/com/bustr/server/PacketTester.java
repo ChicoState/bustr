0caf8cc072b8e7abd8b35ff0fe3a1913775649a3
sterlingbaldwin <sbaldwin4@mail.csuchico.edu>
2014-09-28 10:23:31 -0700

    Adds the PacketTester class.

---
+++ BustrServer/src/com/bustr/server/PacketTester.java
@@ -0,0 +1,47 @@
+package com.bustr.server;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import com.bustr.packets.BustrPacket;
+import com.bustr.packets.ImagePacket;
+import com.bustr.packets.SignalPacket;
+import com.bustr.packets.SignalPacket.BustrSignal;
+
+public class PacketTester {
+	public PacketTester() {
+	}
+	
+	public void sendRequest(){
+		try {
+			Socket sock = new Socket("localhost", 8000);
+			ObjectOutputStream output = new ObjectOutputStream(sock.getOutputStream());
+			ObjectInputStream input = new ObjectInputStream(sock.getInputStream());
+			
+			SignalPacket sp = new SignalPacket(BustrSignal.IMAGE_REQUEST);
+			sp.setLat((float) 39.7276);
+			sp.setLng((float)-121.855);
+			output.writeObject(sp);
+			BustrPacket res = (BustrPacket)input.readObject();
+			if(res instanceof SignalPacket)
+			{
+				SignalPacket inSignal = (SignalPacket)res;
+				if(inSignal.getSignal() == BustrSignal.SUCCESS)
+				{
+					System.out.println("Got a SUCCESS packet");
+				}
+				else if(inSignal.getSignal() == BustrSignal.FAILURE)
+				{
+					System.out.println("Got a FAILURE packet");
+				}
+			}
+			else if(res instanceof ImagePacket)
+			{
+				
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
