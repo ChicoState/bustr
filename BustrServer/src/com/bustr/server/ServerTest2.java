74e13cb3e1e06f3684d85946b55efe42e1b8fcfb
sterlingbaldwin <sbaldwin4@mail.csuchico.edu>
2014-10-01 09:59:54 -0700

    Adds the upLoadTest method

--- BustrServer/src/com/bustr/server/ServerTest2.java
+++ BustrServer/src/com/bustr/server/ServerTest2.java
@@ -2,9 +2,12 @@ package com.bustr.server;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
@@ -17,6 +20,64 @@ import com.bustr.packets.SignalPacket;
 import com.bustr.packets.SignalPacket.BustrSignal;
 
 public class ServerTest2 {
+	
+	private byte[] extractBytes(String ImageName) throws IOException {
+	      // open image
+	      File f = new File(ImageName);
+	      byte[] imageData = new byte[(int) f.length()];
+	      BufferedInputStream bis = new BufferedInputStream(new FileInputStream(f));
+	      bis.read(imageData);
+	      bis.close();
+	      if(imageData == null) System.out.println("[-] Attempting to send packet with empty data.");
+	      return imageData;
+	      // BufferedImaged bufferedImage = ImageIO.read(imgPath);
+	      // get DataBufferBytes from Raster
+	      // WritableRaster raster = bufferedImage.getRaster();
+	      // DataBufferByte data = (DataBufferByte) raster.getDataBuffer();
+	      // return (data.getData());
+	   }
+	
+	@Test
+	public void upLoadTest() {
+		try {
+			String imagePath = "/home/bustr/Desktop/Yoda.jpg";
+			System.out
+					.println("[+] Sending upload packet to server, with imagePath="
+							+ imagePath);
+			Socket sock = new Socket("localhost", 8000);
+			ObjectOutputStream output = new ObjectOutputStream(
+					sock.getOutputStream());
+			ObjectInputStream input = new ObjectInputStream(
+					sock.getInputStream());
+			byte[] imageData = extractBytes(imagePath);
+			ImagePacket ipacket = new ImagePacket("blar", imageData, (float)0.0 ,(float)0.0, "do or do not, there is no try");
+			
+			output.writeObject(ipacket);
+			BustrPacket res;
+			while ((res = (BustrPacket) input.readObject()) != null) {
+
+				if (res instanceof SignalPacket) {
+					SignalPacket inSignal = (SignalPacket) res;
+					if (inSignal.getSignal() == BustrSignal.SUCCESS) {
+						System.out.println("[+] Got a SUCCESS packet");
+						assertTrue(true);
+						return;
+					} else if (inSignal.getSignal() == BustrSignal.FAILURE) {
+						System.out.println("[-] Got a FAILURE packet");
+						assertTrue(true);
+						return;
+					}
+				} else if (res instanceof ImagePacket) {
+					System.out.println("[+] Got an ImagePacket with info "
+							+ res.toString());
+					assertTrue(true);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			assertTrue(false);
+		}
+	}
 
 	@Test
 	public void upVoteTest() {
@@ -117,7 +178,7 @@ public class ServerTest2 {
 			sp.setLng((float) -121.841);
 			output.writeObject(sp);
 			BustrPacket res;
-			int counter = 0;
+			int counter = 0; 
 			while ((res = (BustrPacket) input.readObject()) != null) {
 
 				if (res instanceof SignalPacket) {
