a25a8be13b2132aa684581237aa24ebd9eaa29ec
mt523 <mtostenson@mail.csuchico.edu>
2014-10-02 16:34:45 -0700

    Adds rep int to imagepackets

--- BustrPacket/src/com/bustr/packets/ImagePacket.java
+++ BustrPacket/src/com/bustr/packets/ImagePacket.java
@@ -8,22 +8,33 @@ public class ImagePacket extends BustrPacket {
    private static final long serialVersionUID = 1L;
    private String name;
    private byte[] data;
-   private float  lat, lng;
+   private float lat, lng;
    private String caption;
+   private int rep = 0;
 
    public ImagePacket(String pName, byte[] pData, float pLat, float pLng,
          String pCaption) {
-      name    = pName;
-      data    = pData;
-      lat     = pLat;
-      lng     = pLng;
+      name = pName;
+      data = pData;
+      lat = pLat;
+      lng = pLng;
       caption = pCaption;
    }
-   
+
+   public ImagePacket(String pName, byte[] pData, float pLat, float pLng,
+         String pCaption, int pRep) {
+      name = pName;
+      data = pData;
+      lat = pLat;
+      lng = pLng;
+      caption = pCaption;
+      rep = pRep;
+   }
+
    @Override
-   public String toString(){
-	   String s = name + ": " + lat + " : " + lng + " : " + caption;
-	   return s;
+   public String toString() {
+      String s = name + ": " + lat + " : " + lng + " : " + caption;
+      return s;
    }
 
    public byte[] getData() {
@@ -41,9 +52,13 @@ public class ImagePacket extends BustrPacket {
    public float getLng() {
       return lng;
    }
-   
+
    public String getCaption() {
       return caption;
+   }
+
+   public int getRep() {
+      return rep;
    }
 
 }
