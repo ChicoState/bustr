74db23c5bb3b4b0a58d113fb680a62f1427ab50f
mt523 <mtostenson@mail.csuchico.edu>
2014-10-15 21:57:53 -0700

    Adds image comment functionality

--- bustr/src/com/bustr/fragments/ViewerFragment.java
+++ bustr/src/com/bustr/fragments/ViewerFragment.java
@@ -7,6 +7,8 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
@@ -20,6 +22,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -37,10 +40,11 @@ public class ViewerFragment extends Fragment {
    private static final String LOGTAG = "BUSTR";
    private String imageName;
    private Bitmap image;
+   private String userComment;
 
    // GUI elements -------------------------------------------------------------
    private ViewGroup rootView = null;
-   private Button upvote, downvote;
+   private Button upvote, downvote, comment;
    private TextView viewerCaption;
    private TextView repDisplay;
    private ImageView viewerImage;
@@ -65,26 +69,44 @@ public class ViewerFragment extends Fragment {
       viewerImage = (ImageView) rootView.findViewById(R.id.viewerImage);
       downvote = (Button) rootView.findViewById(R.id.downvote);
       upvote = (Button) rootView.findViewById(R.id.upvote);
+      comment = (Button) rootView.findViewById(R.id.comment);
       OnClickListener voteClick = new OnClickListener() {
          @Override
          public void onClick(View v) {
             if (v.getId() == R.id.upvote) {
                new Voter(BustrSignal.REP_UPVOTE);
-            }
-            else {
+            } else if (v.getId() == R.id.downvote) {
                new Voter(BustrSignal.REP_DOWNVOTE);
+            } else if (v.getId() == R.id.comment) {
+               getCommentFromUser();
             }
          }
       };
       upvote.setOnClickListener(voteClick);
       downvote.setOnClickListener(voteClick);
+      comment.setOnClickListener(voteClick);
       Typeface tf = ResourceProvider.instance(rootView.getContext()).getFont();
       upvote.setTypeface(tf);
       downvote.setTypeface(tf);
+      comment.setTypeface(tf);
       viewerCaption.setTypeface(tf);
       repDisplay.setTypeface(tf);
       new Downloader().execute();
       return rootView;
+   }
+
+   private void getCommentFromUser() {
+      final EditText captionInput = new EditText(rootView.getContext());
+      AlertDialog.OnClickListener listener = new AlertDialog.OnClickListener() {
+         @Override
+         public void onClick(DialogInterface arg0, int arg1) {
+            userComment = captionInput.getText().toString();
+            new Voter(BustrSignal.NEW_COMMENT);
+         }
+      };
+      new AlertDialog.Builder(rootView.getContext()).setTitle("Comment")
+            .setView(captionInput).setNeutralButton("Ok", listener)
+            .setIcon(android.R.drawable.ic_input_get).show();
    }
 
    public void setImage(ImagePacket imagePacket) {
@@ -96,10 +118,10 @@ public class ViewerFragment extends Fragment {
             imagePacket.getData().length);
       Matrix mtx = new Matrix();
       mtx.postRotate(90);
-      float scale = (float)viewerImage.getMeasuredWidth() / image.getWidth();
+      float scale = (float) viewerImage.getMeasuredWidth() / image.getWidth();
       mtx.postScale(scale, scale);
       Bitmap rotated = Bitmap.createBitmap(image, 0, 0, image.getWidth(),
-            image.getHeight(), mtx, true);  
+            image.getHeight(), mtx, true);
       try {
          viewerImage.setImageBitmap(rotated);
       } catch (Exception e) {
@@ -127,9 +149,12 @@ public class ViewerFragment extends Fragment {
             socket = new Socket(InetAddress.getByName("50.173.32.127"), 8000);
             output = new ObjectOutputStream(socket.getOutputStream());
             input = new ObjectInputStream(socket.getInputStream());
-            SignalPacket vote = new SignalPacket(signalType);
-            vote.setImageName(imageName);
-            output.writeObject(vote);
+            SignalPacket packet = new SignalPacket(signalType);
+            packet.setImageName(imageName);
+            if(signalType == BustrSignal.NEW_COMMENT) {
+               packet.setComment(userComment);
+            }
+            output.writeObject(packet);
             response = (SignalPacket) input.readObject();
             output.close();
             input.close();
@@ -149,12 +174,11 @@ public class ViewerFragment extends Fragment {
          super.onPostExecute(result);
          String message = null;
          if (result.getSignal() == BustrSignal.SUCCESS) {
-            message = "successful";
+            message = "Success";
+         } else if (result.getSignal() == BustrSignal.FAILURE) {
+            message = "Fail";
          }
-         else if (result.getSignal() == BustrSignal.FAILURE) {
-            message = "failed";
-         }
-         Toast.makeText(rootView.getContext(), "Vote " + message,
+         Toast.makeText(rootView.getContext(), message,
                Toast.LENGTH_SHORT).show();
       }
    }
