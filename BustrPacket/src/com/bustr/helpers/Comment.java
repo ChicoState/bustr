6e99aa33a2fe76bc1f34b212021f7c6336fbf883
sterlingbaldwin <sbaldwin4@mail.csuchico.edu>
2014-11-10 20:48:21 -0800

    Fixes serializable issue with the Comment.

--- BustrPacket/src/com/bustr/helpers/Comment.java
+++ BustrPacket/src/com/bustr/helpers/Comment.java
@@ -2,37 +2,36 @@ package com.bustr.helpers;
 
 import java.io.Serializable;
 
+public class Comment implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String user;
+	private String body;
+	private String time;
 
-public class Comment implements Serializable{
-   
-   private String user;
-   private String body;
-   private String time;
-   
-   public Comment(String user, String time, String body) {
-      this.user = user;
-      this.time = time;
-      this.body = body;      
-   }
-   
-   public String getUser()
-   {
-	   return user;
-   }
-   
-   public String getBody()
-   {
-	   return body;
-   }
-   
-   public String getTime()
-   {
-	   return time;
-   }
-   
-   @Override
-   public String toString() {
-      return user + "@" + time + ": " + body;
-   }
+	public Comment(String user, String time, String body) {
+		this.user = user;
+		this.time = time;
+		this.body = body;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	@Override
+	public String toString() {
+		return user + "@" + time + ": " + body;
+	}
 }
